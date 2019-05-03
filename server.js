539c0c2673d0e4320609344cf69982aa4eff5943
Lauren Mellon <laumellon@gmail.com>
2019-05-03 13:10:33 -0400

    altered redirect

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 //___________________
 // localhost:3000
 app.get('/' , (req, res) => {
-  res.render('index.ejs');
+  res.redirect('/footwear');
 });
 
 //___________________
